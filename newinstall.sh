b31982a0381bd68667b2e98825596d4275ad03d9
SubZeroJake <jake@spacejake.com>
2013-01-06 15:14:56 -0800

    adds command for symlink to ssd and runlevel check

--- newinstall.sh
+++ newinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$(runlevel | awk '{print $2}')" -eq 2 ]
+then
+    echo "Please run from runlevel 1 with the command \"runlevel 1\""
+    exit
+fi
 sudo apt-get install git vim sshfs -y
 #git clone git://github.com/SubZeroJake/configs.git
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -9,4 +14,5 @@ sudo apt-get install pithos -y
 sudo apt-get install upgrade -y
 sudo apt-get dist-upgrade -y
 sudo /bin/sh -c 'echo "#Move /tmp to RAM\ntmpfs /tmp tmpfs defaults,noexec,nosuid 0 0" >> /etc/fstab'
+for file in `ls -AF ~/ |grep '^\.' |grep -Ev '^\.\./|^\./'`;do mv ${file} /etc/speedy/${USER}/${FILE} && ln -s /etc/speedy/${USER}/${FILE} /home/${USER}/${FILE};done
 sudo vim /etc/fstab
