08a4e0b63e663c23395f9987345321d4c6df1c2b
SubZeroJake <jake@spacejake.com>
2013-01-06 00:10:05 -0800

    Comments line of newinstall.sh that clones repository. It should already exist on the system.

--- newinstall.sh
+++ newinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install git vim sshfs -y
-git clone git://github.com/SubZeroJake/configs.git
+#git clone git://github.com/SubZeroJake/configs.git
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo add-apt-repository ppa:kevin-mehall/pithos-daily -y
